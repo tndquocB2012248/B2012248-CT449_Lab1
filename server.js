63abbc24ab8650c35e702d9ea7166957a4caba2f
tndquocB2012248 <quocb2012248@student.ctu.edu.vn>
2022-09-30 10:00:35 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 const config = require("./app/config");
 
 const PORT = config.app.port;
-app.listenerCount(PORT, ()=> {
+app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}.`);
 });
